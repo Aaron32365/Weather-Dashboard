262f089eaeb869cd4422f2d6d1dad49108c32f1d
Aaron32365 <56372224+Aaron32365@users.noreply.github.com>
2020-02-11 18:20:45 -0800

    additional css added, javascript search functionality added, html adjusted

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,38 @@
-var qeuryUrl = "api.openweathermap.org/data/2.5/weather?"
-var key = "3dd31b39d60866ff9506c1ebf210a5e8"
+    $("#search").on("click", function(event){
+    event.preventDefault()
+    var key = "6c427e9502046eb2009affa9ab9a0d21"
+    var city = $("#cityInput").val().trim();
+    var qeuryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key
+    console.log(qeuryUrl)
+     $.ajax({
+         url: qeuryUrl,
+         method: "GET"
+     })
+     .then(function(response){ 
+        $("#container-right-upper").show()
+         data = response;
+         console.log(data)
+         var iconRef = data.weather[0].icon
+         var tempC = data.main.temp;
+         var temp = ((tempC * 9) / (5 + 32)).toFixed(1)
+         var humid = data.main.humidity
+         var name = data.name
+         var wind = data.wind.speed
 
+         date()
+         $("#nameDate-Today").html(name  + " (" + date + ")" + "<img src='" + "http://openweathermap.org/img/wn/" + iconRef + "@2x.png" + "'" + " alt=''>" )
+         $("#humidity").text("Humidity: " + humid + "%")
+         $("#temp").text("Temperature: " + temp + " F°")
+         $("#wind").text("Wind Speed: " + wind + "MPH")
+         
+     })
+
+})
+function date(){
+    var d = new Date();
+    var n = d.toString()
+    console.log(n)
+    date = n.substring(4,9) + ", " + n.substring(10,15)
+    console.log(date)
+    return date
+ }
