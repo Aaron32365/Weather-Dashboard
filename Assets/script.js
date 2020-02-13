53ac5fe111773f34e81e3b2093c86f67f0e73cc2
Aaron32365 <56372224+Aaron32365@users.noreply.github.com>
2020-02-12 21:01:00 -0800

    additional javascript features added and css fixes

--- Assets/script.js
+++ Assets/script.js
@@ -2,14 +2,15 @@
     event.preventDefault()
     var key = "6c427e9502046eb2009affa9ab9a0d21"
     var city = $("#cityInput").val().trim();
-    var qeuryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key
-    console.log(qeuryUrl)
+    var qeuryUrlDay = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key
      $.ajax({
-         url: qeuryUrl,
+         url: qeuryUrlDay,
          method: "GET"
      })
      .then(function(response){ 
+        check5Day()
         $("#container-right-upper").show()
+        $("#container-right-lower").show()
          data = response;
          console.log(data)
          var iconRef = data.weather[0].icon
@@ -18,21 +19,35 @@
          var humid = data.main.humidity
          var name = data.name
          var wind = data.wind.speed
+         var d = new Date()
+         console.log(d) 
+         date = String(d).slice(4,15)
+         
+        
 
-         date()
-         $("#nameDate-Today").html(name  + " (" + date + ")" + "<img src='" + "http://openweathermap.org/img/wn/" + iconRef + "@2x.png" + "'" + " alt=''>" )
-         $("#humidity").text("Humidity: " + humid + "%")
-         $("#temp").text("Temperature: " + temp + " F°")
-         $("#wind").text("Wind Speed: " + wind + "MPH")
+         $("#nameDate-Today").html(name  + " (" + date + ")" + "<img src='" + "http://openweathermap.org/img/wn/" + iconRef + "@2x.png" + "'" + " alt='Weather icon'>" +"<hr id='top'>" )
+         $("#humidity").attr("class", "text-Style").text("Humidity: " + humid + "%")
+         $("#temp").attr("class", "text-Style").text("Temperature: " + temp + " F°")
+         $("#wind").attr("class", "text-Style").text("Wind Speed: " + wind + "MPH")
          
      })
+     function check5Day(){
+         
+        var qeuryUrl5Day = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + key
+         $.ajax({
+            url: qeuryUrl5Day,
+            method: "GET"
+         }).then(function(response){
+             console.log(response)
+             for(var i = 0; i < response.list.length; i +=8){
+                 var humid = response.list[i].humidity
+                var tempC = response.list[i].main.temp       
+                var temp = ((tempC * 9) / (5 + 32)).toFixed(1)
+                var col = $("<div>")
+                col.attr("class", "col-sm-2").attr("id", "DayCard").html()
+                col.appendTo("#cards")
+             }
+         })
+     }
 
 })
-function date(){
-    var d = new Date();
-    var n = d.toString()
-    console.log(n)
-    date = n.substring(4,9) + ", " + n.substring(10,15)
-    console.log(date)
-    return date
- }
