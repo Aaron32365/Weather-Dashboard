d7a4d78bc8285bf2e53f12cf28575322573cde06
Aaron32365 <56372224+Aaron32365@users.noreply.github.com>
2020-02-25 16:29:34 -0800

    fixed javascript search history bug, cleaned up code

--- Assets/script.js
+++ Assets/script.js
@@ -18,25 +18,10 @@ $("#search").on("click", function(event){   //initilization function
     function start(response){
         if(response != undefined){
             searchHistory() //search history function
-            check5Day() //5-day forecast function
+            check5Day(city) //5-day forecast function
             $("#container-right-upper").show()
-            $("#container-right-lower").show()  //
-            data = response;
-            console.log(data)
-            var iconRef = data.weather[0].icon
-            var tempC = data.main.temp;
-            var temp = ((tempC * 9) / (5 + 32)).toFixed(1)  //grabing weather data
-            var humid = data.main.humidity
-            var name = data.name
-            var wind = data.wind.speed
-            var d = new Date()
-            date = String(d).slice(4,15)//grabbing today's date
-
-            $("#nameDate-Today").html(name  + " (" + date + ")" + "<img src='" + "http://openweathermap.org/img/wn/" + iconRef + "@2x.png" + "'" + " alt='Weather icon'>" +"<hr id='top'>" )//creating full date and corresponding weather icon
-            $("#humidity").attr("class", "text-Style").text("Humidity: " + humid + "%")//humidity
-            $("#temp").attr("class", "text-Style").text("Temperature: " + temp + " F°")//temperature
-            $("#wind").attr("class", "text-Style").text("Wind Speed: " + wind + "MPH")//wind
-    
+            $("#container-right-lower").show()  
+            addToday(response)
         }
         else{
             console.log("error")
@@ -44,6 +29,23 @@ $("#search").on("click", function(event){   //initilization function
         }
     }
 
+    function addToday(response){
+        data = response;
+        //console.log(data)
+        var iconRef = data.weather[0].icon
+        var tempC = data.main.temp;
+        var temp = ((tempC * 9) / (5 + 32)).toFixed(1)  //grabing weather data
+        var humid = data.main.humidity
+        var name = data.name
+        var wind = data.wind.speed
+        var d = new Date()
+        date = String(d).slice(4,15)//grabbing today's date
+
+        $("#nameDate-Today").html(name  + " (" + date + ")" + "<img src='" + "http://openweathermap.org/img/wn/" + iconRef + "@2x.png" + "'" + " alt='Weather icon'>" +"<hr id='top'>" )//creating full date and corresponding weather icon
+        $("#humidity").attr("class", "text-Style").text("Humidity: " + humid + "%")//humidity
+        $("#temp").attr("class", "text-Style").text("Temperature: " + temp + " F°")//temperature
+        $("#wind").attr("class", "text-Style").text("Wind Speed: " + wind + "MPH")//wind
+    }
 
     $(document).on("click", "#history", function(event){ //new API call based off of users search history
         event.preventDefault()
@@ -54,17 +56,19 @@ $("#search").on("click", function(event){   //initilization function
             url: qeuryUrlDay,
             method: "GET"
         })
-        .then(start)
+        .then(check5Day(city))
+        .then(addToday)
     })
 
-     function check5Day(){
+     function check5Day(city){
         var qeuryUrl5Day = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + key
+        console.log(city)
          $.ajax({
             url: qeuryUrl5Day,
             method: "GET"
          }).then(function(response){
              $("#cards").empty()
-             console.log(response)
+            // console.log(response)
              var j = 0;
              var d = new Date()//creating a new date for today
              var date = (String(d).slice(8,10))//getting today's date
@@ -84,13 +88,13 @@ $("#search").on("click", function(event){   //initilization function
          })
         
      }
-            function searchHistory(){
-                var history = $("#recentSearches")
-                $("#recentSearches").empty()
-                for( var i = 0; i < localStorage.length; i++){
-                    history.append("<a href='#' class='col-sm-12' id='history'> " + localStorage.getItem(localStorage.key(i)) + "<br> </a>")//create/append search history
-                }
+    function searchHistory(){
+        var history = $("#recentSearches")
+        $("#recentSearches").empty()
+        for( var i = 0; i < localStorage.length; i++){
+            history.append("<a href='#' class='col-sm-12' id='history'> " + localStorage.getItem(localStorage.key(i)) + "<br> </a>")//create/append search history
         }
+    }
 })
 
 $("#clear").on("click", function(event){//removes seach history
